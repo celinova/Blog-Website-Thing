e66ce512940112ba74ea000c881a04d20263ec65
Celine <celine.piano.21@gmail.com>
2025-07-16 08:59:32 -0400

    Usermodel and Styles

--- Personal_Website/urls.py
+++ Personal_Website/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("sso/", include("django.contrib.auth.urls")),
+    path("", TemplateView.as_view(template_name="homepage.html"), name="home"),
+#    path("", include("pages.urls")),
 ]
--- sso/models.py
+++ sso/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class UserModel(AbstractUser):
+    age = models.PositiveIntegerField(null=True, blank=True)
+    name = models.CharField(max_length=255)
