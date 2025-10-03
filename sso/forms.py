40619f11eb062557768e054ab3d04ced7215cabb
Celine <celine.piano.21@gmail.com>
2025-10-03 13:08:53 -0400

    Removed sso

--- sso/forms.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.forms import UserChangeForm
-from .models import UserModel
-
-class CustomUserChangeForm(UserChangeForm):
-    class Meta:
-        model = UserModel
-        fields = UserChangeForm.Meta.fields
--- sso/models.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-# Create your models here.
-class UserModel(AbstractUser):
-    age = models.PositiveIntegerField(null=True, blank=True)
-    name = models.CharField(max_length=255)
