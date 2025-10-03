555112e88727457f31088ba10795e1228bd8d699
Celine <celine.piano.21@gmail.com>
2025-10-03 13:05:18 -0400

    Added separate app for logins.

---
+++ login/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserChangeForm
+from .models import UserModel
+
+# change subclass later
+class CustomUserAdmin(UserAdmin):
+    form = CustomUserChangeForm
+    model = UserModel
+    list_display = [
+        "email",
+        "username",
+        "age",
+        "name",
+        "is_staff",
+    ]
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("age",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("age",)}),)
+
+
+admin.site.register(UserModel, CustomUserAdmin)
