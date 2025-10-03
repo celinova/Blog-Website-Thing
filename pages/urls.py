79f179658b88b2113960526713845d448aa19ca9
Celine <celine.piano.21@gmail.com>
2025-10-03 13:08:21 -0400

    WIP CRUD operations

--- pages/urls.py
+++ pages/urls.py
@@ -1,1 +1,7 @@
 from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.entries_index, name="index"),
+    path("<str:entry_title>/", views.entry_view, name="entry"),
+]
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Entry
 
-# Create your views here.
+
+def entries_index(request):
+    entries = Entry.objects.all()
+    return render(request, 'entries_index.html', {'entries_index': entries})
+
+
+def entry_view(request, entry):
+    entry_data = get_object_or_404(Entry, entry_title=entry)
+    return render(request, "entry_template.html", {"entry_data": entry_data})
+
+
+@login_required
+def add_entry(request):
+    # retrieve data from the "add page" form
+    if request.method == "POST":
+        input_title = str(request.POST.get('title_field'))
+        input_body = str(request.POST.get('body_field'))
+
+        # check for title collisions, not super inclined to do this by id ;P
+        if input_title in Entry.objects.all():
+            return render(request, "entry_already_exists.html", {"input_title": input_title})
+        else:
+            created_entry = Entry.objects.create(
+                entry_title=input_title, 
+                entry_text=input_body, 
+                publish_date=timezone.now()
+            )
+            return render(request, "entry_added.html", {"created_entry": created_entry})
+
+# @login_required
+# def edit_entry(request):
+#     if request.method == "POST":
+#         return render()
+
+
+@login_required
+def remove_entry(request, entry):
+    entry_data = get_object_or_404(Entry, entry_title=entry)
+    entry_data.delete()
+    return render(request, "entry_deleted.html", {"entry": entry_data})
